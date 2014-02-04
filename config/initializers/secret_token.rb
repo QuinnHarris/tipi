675f60877fd02054e65557b93b643f3d27cfbd08
Quinn Harris <quinn@mountainofpromos.com>
2014-02-03 17:44:11 -0700

    Initial Commit of Rails 4.0.2 using postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Exosphere::Application.config.secret_key_base = '8ceea0ff2fc721d9fa855555ef7e7ee0bb64f16c35189ff918e8ecb81bedce1e7c1ed43f18f6e6b82c560efd668bfbb97f2ee1f629a02662c69e924ccbcdfb52'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Exosphere::Application.config.session_store :cookie_store, key: '_exosphere_session'
