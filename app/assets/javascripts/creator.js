c748d06feb18b3dce23acc88de1d4866138405f6
Jack Scully <jamesjscully@gmail.com>
2014-05-02 22:53:43 -0400

    redo creator

--- app/assets/javascripts/creator.js
+++ app/assets/javascripts/creator.js
@@ -1,20 +1,139 @@
-var g = new dagreD3.Digraph();
+//initialize svg (d3)
+var svg = d3.select("#nodes-container")
+	.append("svg")
+		.attr("pointer-events", "all")
+	.append("g")
+		.attr('transform', 'translate(20,20)')
+		.call(d3.behavior.zoom().on("zoom", rescale));
+function initSvg(){ 	
+	d3.select("#nodes-container")
+		.append("svg")
+			.attr("pointer-events", "all")
+		.append("g")
+			.attr('transform', 'translate(20,20)')
+			.call(d3.behavior.zoom().on("zoom", rescale));
+		}
+
+//initialize Dagre Renderer (dagre)
+var graph = new dagreD3.Digraph();
 var renderer = new dagreD3.Renderer();
 var layout = new dagreD3.layout();
-var relationFrom = null;
-var data = {nodes: [], edges: []};
+
+layout = layout.nodeSep(20).rankSep(20);
 var orientation = "vertical";
-function render(){renderer.layout(layout).nodeSep(20).run(g, d3.select("svg g"));}
+function render(){renderer.layout(layout).run(g, d3.select("svg g"));}
+
+//getData (ajax)
+var data;
+function getData(){
+	graph = new dagreD3.Digraph; //reset the digraph
+	$.getJSON( "data.js", function( data ) {
+		$.each( data.nodes, function( id, name ) {
+  			var inIcon = "src = 'http://i.stack.imgur.com/BUlXq.png'";
+			var projectIcon = "src = 'http://www.endlessicons.com/wp-content/uploads/2013/02/wrench-icon-614x460.png'";
+			var outIcon = "src = 'https://cdn2.iconfinder.com/data/icons/large-glossy-svg-icons/512/logout_user_login_account-512.png'";
+			var title = name;
+			var subtitle ="Subtitle";
+			var format = ["<div class = 'node-outer'><img class = 'node-in' id = ",
+							id, 
+							inIcon, "><img class = 'node-icon' ", 
+							projectIcon, "><div class = 'node-title-area'><div class = 'node-title'>",
+							title,"</div><div class = 'node-subtitle'>",
+							subtitle,"</div></div><img class = 'node-out' id = ",
+							id,
+							outIcon,"></div>"].join('\n');
+			this.value = format;
+  		});
+  		graph.json(data.nodes,data.edges);
+	});
+}
+//sendData (ajax)
+	$.ajax({
+		type: PUSH,
+  		url: "localhost:3000/creator/" + projectName + "/" + projectVersion + "/" + data + ".json",
+  		dataType: "json",
+  		context: document.body,
+	}).done(function() {
+	  	$( this ).addClass( "done" );
+	});
+//updateSvg (Dagre/graphlib)
+
+//updateData (Graphlib)
+
+//appendNode 
+
+//appendEdges
+
+//mouse events
+$(document).ready(function(){
+	$(document).on("click", function (e){
+		e.preventDefault();
+	});
+	initSvg();
+});
+
+//keyboard events
+
+
+//other events
+function toggleOrientation(){
+	if (orientation == "vertical"){
+		layout = layout.rankDir("LR");
+		orientation = "horizontal";
+	}else{
+		layout = layout.rankDir("TB");
+		orientation = "vertical";
+	}
+	render();
+	return;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var g = new dagreD3.Digraph();
+
+var drag_line = svg.append("line")
+    .attr("class", "drag_line")
+    .attr("x1", 0)
+    .attr("y1", 0)
+    .attr("x2", 0)
+    .attr("y2", 0);
+    
+var selectedNode;
+var selectedEdge;
+
+// add keyboard callback
+d3.select(window)
+    .on("keydown", keydown);
+    
+if (d3.event) {
+    // prevent browser's default behavior
+    d3.event.preventDefault();
+  }
 
 var createNode = function(id, name){
-	//var format = [
-	//"<div style = 'background-image: linear-gradient(to right bottom, #5D5AAD 0%, #ACD6E8 85%);'>",
-	//	"<img style = 'height: 1 em;' src= 'https://cdn1.iconfinder.com/data/icons/huge-basic-icons/512/Wrench.png</div>'",
-	//	"<ul class = 'node-title'>#{name}</ul>",
-	//	"<ul class = 'node-buttons'>",
-	//"</div>"].join('\n');
-	g.addNode(id, {label: name});
+
+	g.addNode(id, {label: format});
 };
+
+function initSVG(){
+	d3.select("#nodes-container")
+	.append("svg")
+		.attr("pointer-events", "all")
+	.append("g")
+		.attr('transform', 'translate(20,20)')
+		.call(d3.behavior.zoom().on("zoom", rescale));
+}
 
 function draw(){
 	g = new dagreD3.Digraph;
@@ -46,80 +165,46 @@ function newD(to, from){
 }
 
 function loadProject(){
-	data = { 
-	"nodes": [
-	 {"id":  0, "name": "Start"},
-	 {"id":  1, "name": "Mount Propellers" },
-	 {"id":  2, "name": "Frame"},
-	 {"id":  3, "name": "Program Controller"},
-	 {"id":  4, "name": "Mount Motors"},
-	 {"id":  5, "name": "Mount Flight Controller"},
-	 {"id":  6, "name": "Mount Battery"},
-	 {"id":  7, "name": "Mount Speed Regulator"},
-	 {"id":  8, "name": "Sync Remote Control"},
-	 {"id":  9, "name": "Wire"},
-	 {"id": 10, "name": "Wire"},
-	 {"id": 11, "name": "Wire"},
-	 {"id": 12, "name": "Wire"},
-	 {"id": 13, "name": "Test"},
-	 {"id": 14, "name": "Quadcopter"}
-	],
-	"edges": [
-	 {"id":  0, "to":  0, "from":  1},
-	 {"id":  1, "to":  0, "from":  2},
-	 {"id":  2, "to":  0, "from":  3},
-	 {"id":  4, "to":  1, "from":  9},
-	 {"id":  5, "to":  2, "from":  4},
-	 {"id":  6, "to":  2, "from":  5},
-	 {"id":  7, "to":  2, "from":  6},
-	 {"id":  8, "to":  2, "from":  7},
-	 {"id":  9, "to":  3, "from":  8},
-	 {"id": 10, "to":  4, "from":  9},
-	 {"id": 11, "to":  4, "from": 10},
-	 {"id": 12, "to":  5, "from": 10},
-	 {"id": 13, "to":  5, "from": 11},
-	 {"id": 14, "to":  5, "from": 12},
-	 {"id": 15, "to":  6, "from": 11},
-	 {"id": 16, "to":  7, "from": 12},
-	 {"id": 17, "to":  8, "from": 13},
-	 {"id": 18, "to":  9, "from": 13},
-	 {"id": 19, "to": 10, "from": 13},
-	 {"id": 20, "to": 11, "from": 13},
-	 {"id": 21, "to": 12, "from": 13},
-	 {"id": 22, "to": 13, "from": 14}
-	 ]};
+	
 	draw();
 }
 
-function toggleOrientation(){
-	if (orientation == "vertical"){
-		layout = layout.rankDir("LR");
-		orientation = "horizontal";
-	}else{
-		layout = layout.rankDir("TB");
-		orientation = "vertical";
-	}
-	render();
-	return;
+
+
+function mousedown(){
+	
 }
 
 
-$(document).ready(function(){
-	$(document).on("click", function (e){
-		e.preventDefault();
-	});
-	var overrideLable = window.addLable;
-	window.addLable = function(node, root, marginX, marginY){
-		overrideLable();
-	};
-});
 
 $(document).on('click', '#toggle-direction', toggleOrientation);
 
 $(document).on('click', '#load-project', loadProject);
 
-$(document).on('mousedown', '.node', loadProject);
+$(document).on('mousedown', '.node', mousedown);
 	
+function getData(){
+	
+}
+function updateData(){
+	$.ajax({
+  		type: "POST",
+ 	 	url: "localhost:3000/creator/" + projectName + "/" + projectVersion + "/" + data + ".json",
+  		data: data,
+  		dataType: "json"
+	})
+  	.done(function( msg ) {
+    	alert( "Data Saved: " + msg );
+  	});
+}
+function startDragLine(){
+	
+};
+$(document).on('mousedown', '.node-out', function(e){
+	var target = mousedownNode = e.target.id;
+	startDragLine(target);
+	x1 = 
+});
 
-
-
+function keydown(){}
+*/
