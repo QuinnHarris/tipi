a070976a1b63bebadfc59183b3cce84eec4273fb
Jack Scully <jamesjscully@gmail.com>
2014-06-05 14:40:38 -0400

    panning completed! no known bugs

--- app/assets/javascripts/creator.js
+++ app/assets/javascripts/creator.js
@@ -1,58 +1,57 @@
 /*bugs: 
-	on zoom the html shrinks more than the svg
-*/
+ * 
+ * 
+ */
 
-function edit(){
-	$(function(){
-		$('#doc-container').editable({
-			autosave: true,
-			inlineMode: false,
-			minHeight: '90%',
-			afterSaveCallback: function (data){
-				console.log(data);
-			},
-			});
-	})
+/*features:
+ * 
+ * 		diagram showing cost in money and time
+ * incremental display arrow keys
+ * fill in keydown function
+ * 		undo/redo functionality
+ * 		forking
+ * 		change coloring scheme to :completed(green), :active(red), :available(blue), :unavailable(grey)
+ * 		social media interaction
+ * 		permission
+ * 		if the screen is a smart phone:
+ * 		add moving text if title is to long
+ * 		button for delegate to team
+ * 		double click and activeNode link behavior
+ * 		make data visible to google for seo
+ * 	better node creation
 	
-}
-
-// diagram showing cost in money and time
-// incremental display arrow keys
-// node bank
-// temporary display of active node contents
-	// fill in keydown function
-	// undo/redo functionality
-	// forking
-	// social media interaction
-	// permission
-// project title area and real toolbars
-	// if the screen is a smart phone:
-	// mark completed nodes green
-	// mark available nodes
-	// update svg margins with the d3 example
-	// add scrolling
-	// add moving text if title is to long
-	// button for delegate to team
-	// double click and activeNode link behavior
 //initialize svg (d3)
-var svg;
-var backdrop;
-function initSvg(){ 	
-	svg = d3.select("#nodes-container")
-		.append("svg")
-			.attr("pointer-events", "all")
-		.append("g")
-			.attr('transform', 'translate(20,20)')
-			.call(d3.behavior.zoom().on("zoom", rescale));
-	backdrop = svg.append("rect")
-		.attr("width", '100%')
-		.attr("height", '100%')
-		.attr("fill", 'thistle')
-		.attr('class', 'backdrop')
-		.on('mousemove', mousemove);
-	return svg;
-}
+ * 
+ * 
+ */
 
+var margin = { left: 20, top: 20, bottom: 0, right: 0 };
+var svg;
+var svgg;
+var backdrop;
+function initSvg(){
+	svgNoZoom = d3.select("#svg-container")
+		.append("svg");
+		
+	svg = svgNoZoom
+		.append('g')
+			.call(d3.behavior.zoom()
+				.scaleExtent([1, 10])
+				.on("zoom", zoom))
+			.on("dblclick.zoom", null);
+				
+	backdrop = svg
+		.append('rect')
+			.attr('pointer-events', 'all')
+			.attr("width", '100%')
+			.attr("height", '100%')
+			.attr("fill", 'none');
+			
+	svgg = svg.append("g")
+		.attr('transform', 'translate(' + margin.top +  ',' + margin.left + ')');
+		
+	return svgg;
+}
 //initialize Dagre Renderer (dagre)
 var g = new dagreD3.Digraph();
 var renderer = new dagreD3.Renderer();
@@ -61,22 +60,29 @@ var layout = dagreD3.layout();
 layout = layout.nodeSep(20).rankSep(20);
 var orientation = "vertical";
 
-function transition (selection) {
-	return selection.transition().duration(500);
-}
+function transition (selection) { return selection.transition().duration(500); }
 
 function render(){
 	format();
-	renderer.transition(transition).layout(layout).run(g, d3.select('svg g'));
-	svg.selectAll('.node-outer')
+	renderer
+		.transition(transition)
+		.layout(layout)
+		.run(g, svgg);
+	
+	// post render
+	svgg.selectAll('.node-outer')
 		.call(d3.behavior.drag()
-			.on('dragend', nodeDragend))
+			.on('dragstart', dragstart)
+			.on('dragend', dragend))
     	.on('contextmenu', contextmenu)
     	.on('mousedown', nodeClick);
-    svg.selectAll('node-outer');
-    	
-    	if(typeof activeNode !== 'undefined'){
-		svg.selectAll('#' + activeNode + '.node-outer').classed('active-node', true);
+    
+    // set the active node's class
+    if(typeof activeNode !== 'undefined'){
+		svgg.selectAll('#' + activeNode + '.node-outer').classed('active-node', true);
+		for(i=0;i<nextNodes.length;i++){
+			svgg.selectAll('#' + nextNodes[i] + '.node-outer').classed('next-node', true);
+		}
 	}
 }
 
@@ -108,6 +114,11 @@ $(document).ready(function(){
 	initSvg();
 	backdrop;
 	edit();
+	inter.load();
+	setTimeout(function(){
+		render();
+		setH();
+	}, 1000);
 });
 
 inter = {
@@ -117,7 +128,7 @@ inter = {
 			g = new dagreD3.Digraph();
 			for (i = 0; i < d.length; i++){
 				if (d[i].type == 'node'){
-					var n = g.addNode(null, { id: d[i].id, name: d[i].name });
+					var n = g.addNode(null, { id: d[i].id, name: d[i].name, doc: d[i].doc});
 				}
 				if (d[i].type == 'edge'){
 					var from, to;
@@ -133,9 +144,9 @@ inter = {
 	},
 	addNode: function(name){
 		// user input for name if name isn't given
-		if (typeof name === 'undefined' || typeof name === 'null'){
+		if (typeof name !== 'string'){
 			name = prompt('name your node:');
-			if (typeof name === 'undefined' || typeof name === 'null'){return null;}
+			if (name == null) return;
 		}
 		
 		cid = g.addNode( null, { name: name });
@@ -143,7 +154,7 @@ inter = {
 		return cid;
 	},
 	addEdge: function(to, from){
-		txn = {op: 'add', type: 'edge' };
+		var txn = {op: 'add', type: 'edge' };
 		if (g.node(to).id)
 			txn['v'] = g.node(to).id;
 		else
@@ -162,6 +173,10 @@ inter = {
 		id = g.node(cid).id;
 		g.delNode(cid);
 		inter.ops.push({op: 'remove', type: 'node', id: id});
+		//TODO make sure edges are removed	
+		d3.selectAll('.froala-box').classed("custom-box", true);
+		$('#doc').editable('setHTML', "");
+		delete activeNode;
 	},
 	delEdge: function(e){
 		from = g.node(g.source(e)).id;
@@ -202,9 +217,11 @@ function addEdge(to, from){
 
 function nodeFrom(source, name){
 	var target = inter.addNode(name);
-	inter.addEdge(target, source);
-	inter.run();
-	render();
+	if (typeof target !== 'undefined') {
+		inter.addEdge(target, source);
+		inter.run();
+		render();
+	}
 }
 
 function nodeTo(target, name){
@@ -227,8 +244,6 @@ function delEdge(target, source){
 	render();
 }
 
-
-
 //buttons
 $(document).on('click', '#add-node', addNode); 
 
@@ -236,6 +251,7 @@ $(document).on('click', '#toggle-direction', toggleOrientation);
 
 $(document).on('click', '#load-project', inter.load);
 
+$(window).resize(setH);
 //keyboard call
 d3.select(window)
     .on("keydown", keydown);
@@ -245,9 +261,11 @@ if (d3.event) {
 }
 
 // listener functions
+function dragstart(){
+	d3.event.sourceEvent.stopPropagation();
+}
 
-	//graph states
-function nodeDragend(){
+function dragend(){
 	if(typeof activeNode !== 'undefined'){
 		var abort = false;
 		var source = this.id;
@@ -267,32 +285,53 @@ function nodeDragend(){
 		}
 	}
 }
-function mousemove(){
-	var mouse = d3.mouse(this);
+
+function zoom(){
+	var ev = d3.event;
+	var t = ev.translate;
+	var a = [t[0] + margin.top, t[1] + margin.left];
+	svgg.attr('transform', 'translate(' + a + ') scale(' + ev.scale + ')');
 }
 
 function keydown(){
-	if (typeof activeNode !== 'undefined') {
+	if (typeof activeNode !== 'undefined' && !$('.froala-element').is(':focus'))
 		switch (d3.event.keyCode) {
 	    case 8: // backspace
+	    	d3.event.preventDefault();
+	    	break;
 	    case 46: // delete
+	    	console.log (d3.event.keyCode)
 	        delNode(activeNode);
 			break;
 		case 13: //enter
+			break;
 		case 17: //control
+			break;
 		case 37: // left
+			break;
 		case 38: // up
+			break;
 		case 39: // right
+			break;
 		case 40: // down
+			break;
 		case 90: //z
+			break;
 		case 89: //y
-		
+			break;
 		}
-	}
 }
 function nodeClick(){
+	if( typeof activeNode !== 'undefined')
+		$('#doc').editable('save');
 	activeNode = d3.event.target.id;
+	nextNodes = g.successors(activeNode);
+	$('#doc').editable('setHTML', g.node(activeNode).doc);
+	d3.selectAll('.froala-box').classed("custom-box", false);
+	$('#doc-title').text(g.node(activeNode).name);
+	setH();
 	render();
+	d3.select('.popup').remove();
 }
 
 function contextmenu(){
@@ -300,7 +339,9 @@ function contextmenu(){
     var popup = d3.select(".popup");
     popup.remove();// delete old menu in case it's there
 	id = d3.event.target.id;
-    mousePosition = d3.mouse(backdrop.node()); //map mouse movements
+	console.log(this);
+	console.log(d3.event);
+    mousePosition = d3.mouse(svgNoZoom.node()); //map mouse movements
     //Create the html for the menu
     menu = "<ul class = 'custom-context-menu'>";
     for(i = 0; i < menuData.items.length; i++){
@@ -313,7 +354,7 @@ function contextmenu(){
     
     // Build the popup
 	var w, h;
-    popup = svg.append("foreignObject")
+    popup = svgNoZoom.append("foreignObject")
         .attr("class", "popup")
         .attr("width", 100000);
 
@@ -352,14 +393,25 @@ function contextmenu(){
     //delete context menu on mouse exit
     
     popup
-    .on('mouseleave', function(){
-    	if (d3.event.target.class !== 'custom-context-menu' || d3.event.target.id !== id){
-    		d3.select('.popup').remove();
-    	}
-    })
-    .on('click', function(){
-    	d3.select('.popup').remove();
-    });
+	    .on('mouseleave', function(){
+	    	if (d3.event.relatedTarget.id !== id) d3.select('.popup').remove();
+	    })
+	    .on('click', function(){
+	    	d3.select('.popup').remove();
+	    });
+    
+    d3.selectAll('.node-outer#'+ id)
+	    .on('mouseleave', function(){
+	    	target = d3.event.relatedTarget;
+	    	// check if it is list item
+	    	if (target.className === 'context-menu-item') return;
+	    	//chekc if it is list
+	    	if (target.hasChildNodes() && target.firstElementChild.className === 'custom-context-menu') return;
+	    	else d3.select('.popup').remove();
+	    })
+	    .on('click', function(){
+	    	d3.select('.popup').remove();
+	    });
 }
 
 //DOM interaction functions
@@ -367,15 +419,72 @@ function toggleOrientation(){
 	if (orientation == "vertical"){
 		layout = layout.rankDir("LR");
 		orientation = "horizontal";
+		d3.selectAll('#toggle-direction').classed('fa-arrows-h', false);
+		d3.selectAll('#toggle-direction').classed('fa-arrows-v', true);
 	}else{
 		layout = layout.rankDir("TB");
 		orientation = "vertical";
+		d3.selectAll('#toggle-direction').classed('fa-arrows-v', false);
+		d3.selectAll('#toggle-direction').classed('fa-arrows-h', true);
 	}
 	render();
 	return;
 }
 
-function rescale(){}
+function edit(){
+	$(function(){
+		$('#doc').editable({
+			//autosave: true,
+			//autosaveInterval: 2500,
+			saveURL: dataPath + "/post_doc",
+			inlineMode: false,
+			height: '100%',
+			buttons: ["bold", "italic", "underline",
+				 "strikeThrough", "fontFamily", "fontSize",
+				 "color", "formatBlock", "blockStyle",
+				 "align", "insertOrderedList", 
+				 "insertUnorderedList", "outdent",
+				 "indent", "selectAll", "createLink",
+				 "insertImage", "insertVideo", "undo",
+				 "redo", "html", "save", "insertHorizontalRule"],
+			preloaderSrc: "http://preloaders.net/preloaders/290/Long%20fading%20lines.gif",
+			typingTimer: 750,
+			beforeSaveCallback: function(){
+				if(!activeNode) return
+					docNode = activeNode;
+					$('#doc').editable('option', "saveParams", {version: g.node(activeNode).id});
+			},
+			afterSaveCallback: function (d){
+				g.node(docNode).doc = d.doc;
+				g.node(docNode).id = d.id;
+				if(docNode == activeNode) $('#doc').editable('option', "saveParams", {version: d.id});
+			},
+			saveErrorCallback: function(error){
+				console.log(error);
+			}
+			});
+	});
+}
+
+function setH(){
+	var pad = 12;
+	var buffer = 5;
+	var h = {};
+	h.app = $(window).height()
+		- $(".top-bar").height()
+		- buffer;
+	h.doc = h.app
+		- $(".froala-editor.f-basic").height()
+		- $('#doc-title').height()
+		- pad
+		- buffer;
+	h.svg = h.app
+		- $("#buttons-container").height()
+		- buffer;
+	$('#app-container').height(h.app);
+	$('svg').height(h.svg);
+	$("#doc").height(h.doc);
+}
 
 menuData = {
 	"attr": {
